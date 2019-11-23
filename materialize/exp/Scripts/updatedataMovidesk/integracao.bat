692777d4aa20ea0713bc43df228bbbeff5344909
amaro <arcamos.j@gmail.com>
2019-11-23 14:41:44 -0300

    remove EXP

--- materialize/exp/Scripts/updatedataMovidesk/integracao.bat
+++
@@ -1,4 +0,0 @@
-CHCP 65001
-ECHO Iniciando conexao com o servidor node
-CD "C:\Users\DDIT-03\Documents\Visual Studio 2017\Projects\Canvas - Área do Aluno\DESENVOLVIMENTO\SLCONSTRUCT.UI.WEB\Scripts\scriptCallActionMovidesk\callMethodWithNODEJS" && npm i && node index.js
-exit
